27a1a08727b0cb06a7848db5daf3c807daf9fbdc
Asjadgit <naveedasjad840@gmail.com>
2025-09-24 17:48:54 +0500

    fix: minor fix

--- src/Jobs/PeriodicSynchronizations.php
+++ src/Jobs/PeriodicSynchronizations.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Webkul\Google\Models\Synchronization;
+use Illuminate\Support\Facades\Log;
 
 class PeriodicSynchronizations implements ShouldQueue
 {
@@ -20,6 +21,29 @@ class PeriodicSynchronizations implements ShouldQueue
      */
     public function handle()
     {
-        Synchronization::whereNull('resource_id')->get()->each->ping();
+        Log::info('PeriodicSynchronizations: job started');
+
+        $synchronizations = Synchronization::whereNull('resource_id')->get();
+
+        Log::info('PeriodicSynchronizations: found synchronizations', [
+            'count' => $synchronizations->count(),
+            'ids' => $synchronizations->pluck('id')->toArray(),
+        ]);
+
+        $synchronizations->each(function ($sync) {
+            try {
+                Log::info('PeriodicSynchronizations: pinging synchronization', [
+                    'id' => $sync->id,
+                ]);
+                $sync->ping();
+            } catch (\Throwable $e) {
+                Log::error('PeriodicSynchronizations: failed to ping', [
+                    'id' => $sync->id,
+                    'error' => $e->getMessage(),
+                ]);
+            }
+        });
+
+        Log::info('PeriodicSynchronizations: job finished');
     }
 }
